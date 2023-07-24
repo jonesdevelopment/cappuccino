ef6e45591cb82af366fd2160a41a5a0814a76918
jonesdevelopment <admin@jonesdev.xyz>
2023-07-24 06:46:30 +0200

    build: add build files

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+  id("java")
+  id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+apply(plugin = "java")
+apply(plugin = "com.github.johnrengelman.shadow")
+
+version = "0.1.0"
+
+repositories {
+  mavenCentral() // Lombok
+}
+
+dependencies {
+  compileOnly("org.projectlombok:lombok:1.18.28")
+  annotationProcessor("org.projectlombok:lombok:1.18.28")
+
+  testCompileOnly("org.projectlombok:lombok:1.18.28")
+  testAnnotationProcessor("org.projectlombok:lombok:1.18.28")
+}
+
+tasks {
+  compileJava {
+    options.encoding = "UTF-8"
+  }
+
+  shadowJar {
+    archiveFileName.set("${rootProject.name}.jar")
+  }
+
+  // This is a small wrapper tasks to simplify the building process
+  register("build-cappuchino") {
+    dependsOn("clean", "shadowJar")
+  }
+}
